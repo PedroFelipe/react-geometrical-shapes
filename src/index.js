15d9d2accf0aba04513e83fa5b236eb08758b711
Pedro Felipe <pedro@pedrofelipe.com.br>
2017-10-27 16:20:20 -0300

    Remove initial files; Add Travis

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import reducers from './reducers'
+
+import App from './containers/app/App'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap-theme.css'
+
+import './css/layout.css'
+
+const store = createStore(
+  reducers
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
